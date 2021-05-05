ec2244af7dba6938b277d138eb6400590707f7f3
Sean Dougherty <sean@lupinehook.com>
2021-05-04 18:09:55 -0700

    ship moves, shoots, bouldaries are working....bullets destroy automatically...next up is some enemy action

--- Assets/Code/Player.cs
+++ Assets/Code/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,22 +6,87 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] GameObject playerBlueLazer;
+    [SerializeField] float playerLazerSpeed = 20.0f;
+    [SerializeField] float playerShotTimeBeweenShots = 0.1f;
+
+    Coroutine playerFiringCoroutine;
+
+    // Boundaries for player movement on the board
+    float playerXMin;
+    float playerXMax;
+    float playerYMin;
+    float playerYMax;
+    float playerWidth;
+    float playerHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetPlayerBoundaries();
+        GetPlayerSize();
     }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        PlayerShoot();
+    }
+    private void PlayerShoot()
+    {
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+           playerFiringCoroutine = StartCoroutine(ShootContinuously());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopCoroutine(playerFiringCoroutine);
+        }
+        
+    }
+
+    IEnumerator ShootContinuously()
+    {
+        while (true) 
+        {
+            GameObject playerLazer = Instantiate(playerBlueLazer, transform.position, Quaternion.identity) as GameObject;
+            playerLazer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, playerLazerSpeed);
+
+            yield return new WaitForSeconds(playerShotTimeBeweenShots);
+        }
     }
 
     private void Move()
     {
-        float newXPos = transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float newYPos = transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        float getXPos = transform.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float getYPos = transform.position.y + Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+
+        float newXPos = Mathf.Clamp(getXPos, playerXMin + playerWidth, playerXMax - playerWidth);
+        float newYPos = Mathf.Clamp(getYPos, playerYMin + playerHeight, playerYMax - playerHeight);
+
         transform.position = new Vector2(newXPos, newYPos);
     }
+
+    private void GetPlayerSize()
+    {
+        // used to establish padding. this way no matter what size the player is, the padding will be correct.
+        // half the size of the ship means the nose, wings, and tail will touch the boundaries and not extend.
+        playerHeight = GetComponent<SpriteRenderer>().bounds.size.y / 2;
+        playerWidth = GetComponent<SpriteRenderer>().bounds.size.x / 2;
+    }
+
+    private void SetPlayerBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        
+        playerXMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).x;
+        playerXMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x;
+
+        playerYMin = gameCamera.ViewportToWorldPoint(new Vector3(0,0,0)).y;
+        playerYMax = gameCamera.ViewportToWorldPoint(new Vector3(0,1,0)).y;
+
+    }
+
 }
